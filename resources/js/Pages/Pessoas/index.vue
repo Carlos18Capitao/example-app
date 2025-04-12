02caa92895b7372d3e68bb8ee31091dc999186c2
Carlos Capitão <carlos18mauro@gmail.com>
2025-04-12 12:33:37 +0100

    menu e side bar no layout.

--- resources/js/Pages/Pessoas/index.vue
+++ resources/js/Pages/Pessoas/index.vue
@@ -12,48 +12,30 @@ defineProps({
 
 <template>
     <AppLayout>
-        <!-- Begin Page Content -->
-        <div class="container-fluid">
+        <div class="table-responsive">
+            <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+                <thead>
+                    <tr>
 
-            <!-- Page Heading -->
-            <h1 class="h3 mb-2 text-gray-800">Tables</h1>
-            <p class="mb-4">DataTables is a third party plugin that is used to generate the demo table below.
-                For more information about DataTables, please visit the <a target="_blank"
-                    href="https://datatables.net">official DataTables documentation</a>.</p>
+                        <th>Nome</th>
+                        <th>telefone</th>
 
-            <!-- DataTales Example -->
-            <div class="card shadow mb-4">
-                <div class="card-header py-3">
-                    <h6 class="m-0 font-weight-bold text-primary">DataTables Example</h6>
-                </div>
-                <div class="card-body">
-                    <div class="table-responsive">
-                        <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
-                            <thead>
-                                <tr>
+                        <th></th>
+                    </tr>
+                </thead>
 
-                                    <th>Nome</th>
-                                    <th>telefone</th>
+                <tbody>
+                    <tr v-for="pessoa in pessoas" :key="pessoa.id">
+                        <td>{{ pessoa.nome }}</td>
+                        <td>{{ pessoa.telefone }}</td>
+                        <td>
+                            <a href="#" class="btn btn-primary mr-2">Editar</a>
+                            <a href="#" class="btn btn-danger">Excluir</a>
+                        </td>
+                    </tr>
 
-                                    <th></th>
-                                </tr>
-                            </thead>
-
-                            <tbody>
-                                <tr v-for="pessoa in pessoas" :key="pessoa.id">
-                                    <td>{{ pessoa.nome }}</td>
-                                    <td>{{ pessoa.telefone }}</td>
-                                    <td>
-                                        <a href="#" class="btn btn-primary mr-2">Editar</a>
-                                        <a href="#" class="btn btn-danger">Excluir</a>
-                                    </td>
-                                </tr>
-
-                            </tbody>
-                        </table>
-                    </div>
-                </div>
-            </div>
+                </tbody>
+            </table>
         </div>
     </AppLayout>
 </template>
